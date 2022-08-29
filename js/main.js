09217be0a64006c28a1a485faef47cf1bf79de8a
Bereg <atlant202009@gmail.com>
2022-08-30 00:28:00 +0300

    else

--- js/main.js
+++ js/main.js
@@ -1,17 +1,74 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', "");
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-const a = prompt('Один из последних просмотренных фильмов?', ""),
-    b = prompt('На сколько оцените его?', ""),
-    c = prompt('Один из последних просмотренных фильмов?', ""),
-    d = prompt('На сколько оцените его?', "");
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', "");
+// const personalMovieDB = {
+//     count: numberOfFilms,
+// //     movies: {},
+// //     actors: {},
+// //     genres: [],
+// //     privat: false
+// // };
+// // const a = prompt('Один из последних просмотренных фильмов?', ""),
+// //     b = prompt('На сколько оцените его?', ""),
+// //     c = prompt('Один из последних просмотренных фильмов?', ""),
+// //     d = prompt('На сколько оцените его?', "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// // personalMovieDB.movies[a] = b;
+// // personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// // console.log(personalMovieDB);
+// function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+// //   // Change code below this line
+// //  const message = "You ordered droids worth ${orderedQuantity * pricePerDroid + deliveryFee} \
+// //      credits. Delivery (${deliveryFee} credits) is included in total price.";
+
+// // makeOrderMessage(2, 100, 50);
+// // makeOrderMessage(4, 300, 100);
+// // makeOrderMessage(10, 70, 200);
+// //   // Change code above this line
+// //   return message;
+// // }
+// function getShippingCost(country) {
+//   let message;
+//   // Change code below this line
+//     switch (country) {
+//     case "China":
+//             message = 'Shipping to China will cost 100 credits';
+//     break;
+        
+//     case "Chile":
+//             message = 'Shipping to Chile will cost 250 credits';
+//     break;
+        
+//     case "Australia":
+//             message = 'Shipping to Australia will cost 170 credits';
+//     break;
+        
+//     case "Jamaica":
+//             message = 'Shipping to Jamaica will cost 120 credits';
+//     break;
+        
+//   default:
+//     message = 'Sorry, there is no delivery to your country';
+// }
+//   // Change code above this line
+//   return message;
+// }
+// const clients = ["Mango", "Ajax", "Poly"];
+
+// for (const client of clients) {
+//   console.log(client);
+// }
+// const string = "javascript";
+
+// for (const character of string) {
+//   console.log(character);
+// }
+// const planets = ["Earth", "Mars", "Venus"];
+
+// for (let i = 0; i < planets.length; i += 1) {
+// //   console.log(planets[i]);
+// // }
+// const clients = ["Mango", "Ajax", "Poly"];
+
+// for (const client of clients) {
+//   console.log(client);
+// }
